1629299fa243f3a2a9c050aca25ab459513c3815
YourGitHubUsername <dipshikapradhan2201@gmail.com>
2024-10-03 03:17:10 +0530

    form changes

--- server.js
+++ server.js
@@ -596,6 +596,7 @@ app.put("/orders/:orderNo/escalation", async (req, res) => {
     const updateData = req.body; 
     console.log("updatedData", updateData);
     var orderNo = updateData.orderNo;
+    console.log("orderNo",orderNo);
   const order = await Order.findOne({ orderNo }); 
   const yardIndex = updateData.yardIndex;
   var actualYardIndex = yardIndex - 1;
