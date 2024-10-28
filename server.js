113807355f54fc6b32d4e48ee7369d6b23271a92
YourGitHubUsername <dipshikapradhan2201@gmail.com>
2024-10-28 19:37:24 +0530

    form changes

--- server.js
+++ server.js
@@ -1168,7 +1168,7 @@ pass: "hweg vrnk qyxx gktv",
 });
 const mailOptions = {
   from: "service@50starsautoparts.com",
-  to: `dipsikha.spotopsdigital@gmail.com`,
+  to: `${order.email},50starsautoparts01@gmail.com`,
   subject: `Tracking Details / Order No. ${req.params.orderNo}`,
   html: `<p>Hi ${order.customerName},</p>
   <p>This email is regarding the order you placed with 50 stars auto parts, and we have attached the tracking information in the same email along with a link that will take you directly to the tracking page.</p>
