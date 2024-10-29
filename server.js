eaf2cd780197787d29777ca755f84348c9251a67
YourGitHubUsername <dipshikapradhan2201@gmail.com>
2024-10-29 20:01:56 +0530

    form changes

--- server.js
+++ server.js
@@ -1165,7 +1165,7 @@ const mailOptions = {
   bcc:`dipsikha.spotopsdigital@gmail.com,service@50starsautoparts.com`,
   subject: `Tracking Details / Order No. ${req.params.orderNo}`,
   html: `<p>Hi ${order.customerName},</p>
-  <p>This email is regarding the order you placed with 50 stars auto parts, and we have attached the tracking information in the same email along with a link that will take you directly to the tracking page.</p>
+  <p>This email is regarding the order you placed with <b>50 Stars Auto Parts</b>, and we have attached the tracking information in the same email along with a link that will take you directly to the tracking page.</p>
   <p>If the ETA is not updated in the system, it may take 24 hours to reflect on the tracking website, you may check again if you do not find the ETA.</p>
   <p>Please call us if you have any questions.</p>
   <p>${shipperName} - ${trackingNo}</p>
