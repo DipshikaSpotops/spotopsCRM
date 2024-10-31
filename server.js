ef63314814f5e0312c1c1580aad174571ed206d7
YourGitHubUsername <dipshikapradhan2201@gmail.com>
2024-10-31 20:57:37 +0530

    form changes

--- server.js
+++ server.js
@@ -2357,7 +2357,7 @@ cid: 'logo'
   
   });
   } catch (error) {
-    console.log("Error ")
+    console.log("Error gggg ")
   console.error("Server error:  hhh", error);
   res.status(500).json({ message: "Server error", error });
   }
