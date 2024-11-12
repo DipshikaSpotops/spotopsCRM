ad3ea264de7cb0cd88522e21aced25ddba6d7f9a
YourGitHubUsername <dipshikapradhan2201@gmail.com>
2024-11-12 20:47:07 +0530

    form changes

--- server.js
+++ server.js
@@ -372,12 +372,14 @@ const orders = await Order.find({});
 res.json(orders);
 });
 // orders per page fpr server side pagination
-app.get("/ordersPerpage", async (req, res) => {
+app.get("/ordersPerPage", async (req, res) => {
   try {
-    const page = parseInt(req.query.page) || 1; 
-    const limit = parseInt(req.query.limit) || 25; 
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 25;
 
+    // Fetch orders sorted by date in descending order
     const orders = await Order.find()
+      .sort({ orderDate: -1 }) // -1 for descending order
       .skip((page - 1) * limit)
       .limit(limit);
 
@@ -392,6 +394,7 @@ app.get("/ordersPerpage", async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
+
 
 // for only placed orders
 app.get("/orders/placed", async (req, res) => {
