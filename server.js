60aba1b6c01816b6689b17ee66c8d982b613847e
YourGitHubUsername <dipshikapradhan2201@gmail.com>
2025-01-24 01:11:00 +0530

    form changes

--- server.js
+++ server.js
@@ -1115,8 +1115,8 @@ async function updateTaskStatuses() {
 
         // Handle "New task added" -> "Processing" after 5 minutes
         if (task.taskStatus === "New task added") {
-          console.log("New",currentDallasTime,createdTime)
-          if (moment(currentDallasTime).diff(createdTime, "minutes") >= 5) {
+          console.log("New",currentDallasTime,createdTime.format("YYYY-MM-DDTHH:mm:ss"))
+          if (moment(currentDallasTime).diff(createdTime.format("YYYY-MM-DDTHH:mm:ss"), "minutes") >= 5) {
             task.taskStatus = "Processing";
             isUpdated = true;
 
