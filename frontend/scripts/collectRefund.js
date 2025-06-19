fd2f0da529e5253d6fde6978929eb80a1caf9790
Dipshika01 <dipshikapradhan2201@gmail.com>
2025-06-20 00:08:28 +0530

    form changes

--- frontend/scripts/collectRefund.js
+++ frontend/scripts/collectRefund.js
@@ -121,19 +121,10 @@ $("#viewAlltasks").on("click", function () {
   .html(currentSort.order === "asc" ? "&#9650;" : "&#9660;"); // Toggle current column icon
   }
   );
-  $(document).on("click", "#infoTable tr", function () {
+$(document).on("click", "#yardInfoTable tr", function () {
   const isSelected = $(this).hasClass("selected");
 
-  $("#infoTable tr").removeClass("selected");
-
-  if (!isSelected) {
-    $(this).addClass("selected");
-  }
-});
-$(document).on("click", "#infoTable tr", function () {
-  const isSelected = $(this).hasClass("selected");
-
-  $("#infoTable tr").removeClass("selected");
+  $("#yardInfoTable tr").removeClass("selected");
 
   if (!isSelected) {
     $(this).addClass("selected");
