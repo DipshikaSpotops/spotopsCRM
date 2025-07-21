060707bf8d4189f80c5eed7776de128bce3fa604
Dipshika01 <dipshikapradhan2201@gmail.com>
2025-07-21 18:56:30 +0530

    making endpoint changes for /disputes and /refunde

--- frontend/scripts/refundedOrders.js
+++ frontend/scripts/refundedOrders.js
@@ -163,7 +163,7 @@ function renderTableRows(page, orders = allOrders) {
             escalationStyle = 'style="background-color: lightgreen;"';
         }
 
-        const team = localStorage.getItem('team');
+        var team = localStorage.getItem('team');
      
         const actions = `
             <button class="btn btn-success btn-sm process-btn" data-id="${item.orderNo}" ${item.orderStatus === "Placed" || item.orderStatus === "Customer approved" ? "disabled" : ""}>View</button>`;
