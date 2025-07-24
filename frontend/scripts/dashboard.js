60b7431bbaa5365db805816c62d2cd7d2aae535b
Dipshika01 <dipshikapradhan2201@gmail.com>
2025-07-25 01:48:47 +0530

    form changes

--- frontend/scripts/dashboard.js
+++ frontend/scripts/dashboard.js
@@ -1356,6 +1356,7 @@ const handleMigrateDates = async () => {
 fetchNotifications();
 await fetchAndRenderCharts()
 document.getElementById("downloadCsvBtn").addEventListener("click", async () => {
+  console.log("Download CSV button clicked");
     try {
       const response = await fetch("https://www.spotops360.com/export-json");
 
