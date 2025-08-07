46f9fdbf90bb415cb878e9310f86975f9bc9a370
Dipshika01 <dipshikapradhan2201@gmail.com>
2025-08-08 02:06:55 +0530

    refund email changes

--- frontend/scripts/form.js
+++ frontend/scripts/form.js
@@ -2357,9 +2357,9 @@ if (!file) {
 console.log("yardddd",selectedYardIndex,yardIndex);
 const formData = new FormData();
 formData.append("pdfFile", file);
-formData.append("firstName", firstName);
+// formData.append("firstName", firstName);
 console.log("firstName",firstName);
-fetch(`https://www.spotops360.com/orders/sendRefundEmailYard/${orderNo}?yardIndex=${selectedYardIndex}&returnTracking=${returnTracking}&refundToCollect=${refundToCollect}&shipper=${shipper}&refundReason=${refundReason}`, {
+fetch(`https://www.spotops360.com/orders/sendRefundEmailYard/${orderNo}?yardIndex=${selectedYardIndex}&returnTracking=${returnTracking}&refundToCollect=${refundToCollect}&shipper=${shipper}&refundReason=${refundReason}&firstName=${firstName}`, {
 method: "POST",
 body: formData,
 
